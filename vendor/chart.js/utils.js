26853e5bf56f5db31bb6b452b9ca61c5dc01bf4c
Carlitosry <hola@carlitosry.com>
2018-03-26 18:26:54 +0200

    - Creacion de fuente en Icon moon que no estaban en Google font. - Corrección de Medias Querys - estructura resposive para dispositivos mobiles

--- vendor/chart.js/utils.js
+++ vendor/chart.js/utils.js
@@ -1,11 +1,11 @@
 'use strict';
 
 window.chartColors = {
-	red: '#e17170',
+	red: 'rgb(255, 99, 132)',
 	orange: 'rgb(255, 159, 64)',
 	yellow: 'rgb(255, 205, 86)',
-	green: '#9cde97',
-	blue: '#007bff',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
 	purple: 'rgb(153, 102, 255)',
 	grey: 'rgb(201, 203, 207)'
 };
@@ -132,127 +132,6 @@ window.chartColors = {
 
 	Samples.utils.srand(Date.now());
 
-	// Google Analytics
-	/* eslint-disable */
-	if (document.location.hostname.match(/^(www\.)?chartjs\.org$/)) {
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-28909194-3', 'auto');
-		ga('send', 'pageview');
-	}
-	/* eslint-enable */
-
 }(this));
 
-
-var randomScalingFactor = function() {
-    return Math.round(Math.random() * 100);
-};
-
-var config = {
-    type: 'doughnut',
-    data: {
-        datasets: [{
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-            backgroundColor: [
-                window.chartColors.gray,
-                window.chartColors.green,
-                window.chartColors.blue
-            ],
-            label: false
-        }],
-        labels: [
-            "Gray",
-            "Green",
-            "Blue"
-        ]
-    },
-    options: {
-        responsive: true,
-        legend: {
-            display: false,
-        },
-        title: {
-            display: false,
-		},
-        animation: {
-            animateScale: true,
-            animateRotate: true
-        }
-    }
-};
-
-var config1 = {
-    type: 'doughnut',
-    data: {
-        datasets: [{
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-            backgroundColor: [
-                window.chartColors.red,
-                window.chartColors.green,
-                window.chartColors.blue
-            ],
-            label: false
-        }],
-        labels: [
-            "Red",
-            "Green",
-            "Blue"
-        ]
-    },
-    options: {
-        responsive: true,
-        legend: {
-            display: false
-        },
-        title: {
-            display: false
-        },
-        animation: {
-            animateScale: true,
-            animateRotate: true
-        }
-    }
-};
-
-var config2 = {
-    type: 'doughnut',
-    data: {
-        datasets: [{
-            data: [
-                randomScalingFactor()
-            ],
-            backgroundColor: [
-                window.chartColors.red,
-            ],
-            label: false
-        }],
-        labels: [
-            "Red"
-        ]
-    },
-    options: {
-        responsive: true,
-        legend: {
-            display: false
-        },
-        title: {
-            display: false
-        },
-        animation: {
-            animateScale: true,
-            animateRotate: true
-        }
-    }
-};
 
